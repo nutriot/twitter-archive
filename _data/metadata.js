89ba8be53a5e880dca06501bdd791899122fb3ed
Jan T. Sott <git@idleberg.com>
2022-11-22 10:48:46 +0100

    adjust url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
 	username: "nutriot", // No leading @ here
 	homeLabel: "Home",
-	homeUrl: "https://nutriot.github.com/twitter-archive",
+	homeUrl: "https://nutriot.github.io/twitter-archive",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
